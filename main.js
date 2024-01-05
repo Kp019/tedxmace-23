d49b46d547750cf73d2f7cd56d30fa2f84b6315d
kp019 <krishnaprasadr666@gmail.com>
2024-01-05 20:52:19 +0530

    routing pages on vite

--- main.js
+++ main.js
@@ -7,6 +7,8 @@ const CollectionId = import.meta.env.VITE_COLLECTIONID
 const ProjectId = import.meta.env.VITE_PROJECTID
 const DataBaseId = import.meta.env.VITE_APP_DATAID
 const EndPoint =  import.meta.env.VITE_ENDPOINT
+const Collection_early = import.meta.env.VITE_APP_COLLECTION_EARLY
+
 
 client
     .setEndpoint(EndPoint)
@@ -14,8 +16,50 @@ client
 
 const db = new Databases(client)
 
-const form = document.getElementById('myform');
-form.addEventListener('submit',submitform);
+const form = document.querySelector('#myform');
+if(form){
+    form.addEventListener('submit',submitform);
+}
+
+const formearly = document.querySelector('#myformearly');
+if(formearly){
+    formearly.addEventListener('submit', submitearly);
+}
+
+
+async function submitearly(e){
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const phone = e.target.phn.value;
+    const org = e.target.district.value;
+    const orgname = e.target.org_name.value;
+    const link = e.target.proof.value;
+    console.log(name)
+    const erly_resp = await db.createDocument(
+        DataBaseId,
+        Collection_early,
+        ID.unique(),
+        {
+            'name': name,
+            'number': phone,
+            'email': email,
+            'organization': org,
+            'orgname': orgname,
+            'proof': link,
+        }
+    )
+    
+    .then(erly_resp => {
+        formearly.reset();
+        alert('registration Successful');
+    })
+
+    .catch(error => {
+        alert('Error OCCURED CONTACT AT : +91 8330859663');
+        console.error('error occured')
+    });
+}
 
 async function submitform(e){
     e.preventDefault();
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import dotenv from 'dotenv';
+import {resolve} from 'path'
 
 export default defineConfig({
     define:{
@@ -7,9 +8,22 @@ export default defineConfig({
         'process.meta.env.VITE_COLLECTIONID':JSON.stringify(process.env.VITE_COLLECTIONID),
         'process.meta.env.VITE_PROJECTID':JSON.stringify(process.env.VITE_PROJECTID),
         'process.meta.env.VITE_ENDPOINT':JSON.stringify(process.env.VITE_ENDPOINT),
+        'process.meta.env.VITE_COLLECTION_EARLY':JSON.stringify(process.env.VITE_COLLECTION_EARLY),
+        'process.meta.env.VITE_COLLECTION_GROUP':JSON.stringify(process.env.VITE_COLLECTION_GROUP),
+        'process.meta.env.VITE_COLLECTION_MACE':JSON.stringify(process.env.VITE_COLLECTION_MACE),
+        'process.meta.env.VITE_COLLECTION_MACEP':JSON.stringify(process.env.VITE_COLLECTION_MACEP),
+        'process.meta.env.VITE_COLLECTION_PREMIUM':JSON.stringify(process.env.VITE_COLLECTION_PREMIUM),
         
-    }
+    },
+
+    build: {
+        rollupOptions: {
+            input: {
+            main: resolve(__dirname, 'index.html'),
+            registernorm: resolve(__dirname, 'pages/registernorm/index.html'),
+            registerearly: resolve(__dirname, 'pages/registerearly/index.html'),
+          },
+        },
+      },
 })
-
-
 
